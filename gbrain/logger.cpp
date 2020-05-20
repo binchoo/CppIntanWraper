900d8c7489ea7d1ab09e5face61f03806d88be2e
binchoo <079111w@gmail.com>
2020-05-20 14:36:52 +0900

    loggerfix

--- gbrain/logger.cpp
+++ gbrain/logger.cpp
@@ -3,16 +3,17 @@
 
 Logger* Logger::instance = nullptr;
 
-Logger* Logger::getInstance() {
+Logger& Logger::getInstance() {
 	if (Logger::instance == nullptr) {
 		Logger::instance = &Logger();
 	}
-	return Logger::instance;
+	return *Logger::instance;
 }
 
-void Logger::log(string tag, string message) {
+void Logger::commit(string tag) {
 	ofstream file = ofstream("log.txt", ios::app);
-	file << '[' << tag << '|' << currentDateTime() << ']' << message << endl;
+	file << '[' << tag << '|' << currentDateTime() << ']' << ss.str() << endl;
+	ss.clear();
 	file.close();
 }
 
@@ -26,3 +27,21 @@ string Logger::currentDateTime() {
 
 	return buf;
 }
+
+ostream& operator<<(Logger& logger, const string s)
+{
+	logger.ss << s ;
+	return logger.ss;
+}
+
+ostream& operator<<(Logger& logger, const int i)
+{
+	logger.ss << i;
+	return logger.ss;
+}
+
+ostream& operator<<(Logger& logger, std::ios_base& base)
+{
+	logger.ss << base;
+	return logger.ss;
+}) 
--- gbrain/logger.h
+++ gbrain/logger.h
@@ -2,16 +2,22 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <sstream>
 using namespace std;
 
 class Logger {
 private:
 	Logger() {
+		ss = stringstream();
 	};
 	Logger(const Logger& other);
 	static Logger* instance;
 	string currentDateTime();
+	stringstream ss;
 public:
-	static Logger* getInstance();
-	void log(string tag, string message);
+	static Logger& getInstance();
+	void commit(string tag);
+	friend ostream& operator<<(Logger& logger, const string s);
+	friend ostream& operator<<(Logger& logger, const int s);
+	friend ostream& operator<<(Logger& logger, std::ios_base& );
 };
